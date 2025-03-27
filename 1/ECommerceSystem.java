e5f59154800887e823d7265b88defb257cf32d84
DivyaUtla <divyachowdaryb0@gmail.com>
2025-03-27 11:55:56 -0700

    added comments

--- 1/ECommerceSystem.java
+++ 1/ECommerceSystem.java
@@ -12,27 +12,27 @@ class Product{
         this.price=price;
     }
 
-    public String toString(){
+    public String toString(){ //used toString() to display product details in a readable format
         return "Product: [ "+name+" Price: "+price + " ]";
     }
 }
 
 class Cart{
-        List<Product> cartList=new ArrayList<>();
+        List<Product> cartList=new ArrayList<>(); // created carList to hold the products
 
         public void addProduct(Product item){
-            cartList.add(item);
+            cartList.add(item); // adds the products to cart List
         }
 
         public double calculateTotalBill(){
             double total=0;
-            for(Product item:cartList){
-                total+=item.price;
+            for(Product item:cartList){ // this will through all the items in the Cart List
+                total+=item.price; // adds the price of each prod to total and return the same
             }
             return total;
         }
         
-        public void showCart(){
+        public void showCart(){ // want to show all the products that are added in Cart List
             System.out.println("Cart Items: ");
             for(Product p:cartList){
                 System.out.println(p);
@@ -48,13 +48,13 @@ class Customer{
     public Customer(int id, String name){
         this.id=id;
         this.name=name;
-        this.cart=new Cart();
+        this.cart=new Cart(); // created cart for customer
     }
 
     public Order placeOrder(){
         System.out.println("Customer Name : " + name);
         cart.showCart();
-        Order newOrder = new Order(this);
+        Order newOrder = new Order(this); // created new order for current customer
         System.out.println("Order placed Successfully");
         return newOrder;
     }
@@ -63,13 +63,13 @@ class Customer{
 
 class Order{
     //int id;
-    Customer customer;
-    List<Product> orderedItems;
+    Customer customer; // The customer placing the order
+    List<Product> orderedItems; //list of ordered product items by customer
     double totalBill;
     
     public Order(Customer customer){
         //this.id=customer.id;
-        this.customer=customer;
+        this.customer=customer; 
         this.orderedItems=new ArrayList<>(customer.cart.cartList);
         this.totalBill=customer.cart.calculateTotalBill();
     }
