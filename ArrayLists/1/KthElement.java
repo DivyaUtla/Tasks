c17f368ab355054c013a0a52a813ab1273ce9482
DivyaUtla <divyachowdaryb0@gmail.com>
2025-04-01 11:26:55 -0700

    added edge case

--- ArrayLists/1/KthElement.java
+++ ArrayLists/1/KthElement.java
@@ -1,22 +1,24 @@
 //ASK : Find the kth smallest element in an ArrayList
 
-import java.util.Arrays;
+//import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class FindSmallest{
-    public static int FindKthSmallest(int[] arr, int k){
+    public static int FindKthSmallest(ArrayList<Integer> arr, int k){
         //Adding Edge case
-        if(k<1 || k>arr.length){
+        if(k<1 || k>arr.size()){
             throw new IllegalArgumentException("K value is not valid");
         }
 
-        Arrays.sort(arr);
-        return arr[k-1];
+        Collections.sort(arr);
+        return arr.get(k-1);
         
         //im sorting arrays in ascending order
 
         // since array starts with 0 nexts i want to find Kth smallest element so used K-1 
-        //Example: int[] arr ={23,3,89,7,56,99,1}; and want to find K=4 smasllest element
+        //Example:  arr ={23,3,89,7,56,99,1}; and want to find K=4 smasllest element
         //arr={1,3,7,23,56,89,99} so 4th smallest element is 23 so arr[3]=23
         //if K=1 then arr[0]=1
         //if K=2 then arr[1]=3
@@ -37,10 +39,10 @@ public class KthElement{
        Scanner sc=new Scanner(System.in);
        System.out.println("Enter the size of the array : ");
        int n=sc.nextInt();
-       int[] arr=new int[n];
+       ArrayList<Integer> arr=new ArrayList<>(n);
        System.out.println("Enter the elements of the array : ");
        for(int i=0;i<n;i++){
-        arr[i]=sc.nextInt();
+        arr.add(sc.nextInt());
        }
        System.out.println("Enter K value : ");
        int k=sc.nextInt();
