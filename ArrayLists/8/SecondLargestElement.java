70ed3ac0580f51a7001b7f89b1a5c04bc7b45cab
DivyaUtla <divyachowdaryb0@gmail.com>
2025-04-02 17:38:53 -0700

    created methods to find second large element in given list

---
+++ ArrayLists/8/SecondLargestElement.java
@@ -0,0 +1,35 @@
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Largest{
+    public static int LargestElement(List<Integer> list){
+        List<Integer> newList=new ArrayList<>(list);// created newList to store the elements from list
+        Collections.sort(newList, Collections.reverseOrder()); // i want the elemnts to be sorted in descending order so used Collections.reverseOrder()
+    return newList.get(1); //since i need the second largest elements so im returning index 1
+    }
+}
+
+public class SecondLargestElement{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter size of list : ");
+        int n=sc.nextInt();
+
+        List<Integer> list=new ArrayList<>();
+        System.out.println("Enter Elements of the List : ");
+        for(int i=0;i<n;i++){
+            list.add(sc.nextInt());
+        }
+
+        int result=Largest.LargestElement(list);
+        System.out.println("The second Largest Element in the given List : " + result);
+
+
+        //List<Integer> list=List.of(1,33,14,118,4);
+        //int result=Largest.LargestElement(list);
+        //System.out.println(result);
+    }
+}
